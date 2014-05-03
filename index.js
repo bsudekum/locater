37d5f3472f6274fb43f86f9adbdb8818199787c6
rsudekum <bobby@mapbox.com>
2014-05-03 16:39:42 -0700

    fix port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 var Static = require('node-static');
 var app = http.createServer(handler);
 var io = require('socket.io').listen(app);
-var port = 5000;
+var port = Number(process.env.PORT || 5000);
 
 var files = new Static.Server('./public');
 
